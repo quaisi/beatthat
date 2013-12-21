d0a232f29479384b88aa8a306d45fac755ec1bc0
Simon <simon.quain@gmail.com>
2013-12-21 19:07:09 +0000

    Only allows numerical-only input as anwers

--- beatthat.pl
+++ beatthat.pl
@@ -8,7 +8,7 @@ say "******************************BEAT THAT******************************";
 
 my $total = 0;
 my $again = 1;
-
+my $valid_input = 0;
 
 while ($again == 1){
     for (1..10){
@@ -17,16 +17,21 @@ while ($again == 1){
         my $rand2 = int(rand(10) + 1);
         my $answer = $rand1 * $rand2;
 
-        print "What is $rand1 x $rand2: ";
-        chomp (my $input = <STDIN>);
-        if ($input == $answer) {
-            $total ++;
-        }
-    }
+        while ($valid_input == 0) {
+            print "What is $rand1 x $rand2: ";
+            chomp (my $input = <STDIN>);
+            if ($input =~ /\A(\d+)\z/){
+                $valid_input = 1;
+                if ($input == $answer) {
+                    $total ++;
 
+                }
+            }
+        }
+        $valid_input = 0;
+    }
     say "You got $total out of 10 correct";
     say "Type yes to go again: ";
     chomp (my $input = <STDIN>);
     $again = 0 unless $input =~ /yes/i;
 }
-
