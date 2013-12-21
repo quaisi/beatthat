c1d208a03af01dc2be57f9642a28232d794d82cf
Simon <simon.quain@gmail.com>
2013-12-21 18:40:31 +0000

    Prompts to go again

--- beatthat.pl
+++ beatthat.pl
@@ -6,19 +6,27 @@ use 5.010;
 
 say "******************************BEAT THAT******************************";
 
-my $total;
+my $total = 0;
+my $again = 1;
 
-for (1..10){
 
-    my $rand1 = int(rand(10) + 1);
-    my $rand2 = int(rand(10) + 1);
-    my $answer = $rand1 * $rand2;
+while ($again == 1){
+    for (1..10){
 
-    print "What is $rand1 x $rand2: ";
-    chomp (my $input = <STDIN>);
-    if ($input == $answer) {
-        $total ++;
+        my $rand1 = int(rand(10) + 1);
+        my $rand2 = int(rand(10) + 1);
+        my $answer = $rand1 * $rand2;
+
+        print "What is $rand1 x $rand2: ";
+        chomp (my $input = <STDIN>);
+        if ($input == $answer) {
+            $total ++;
+        }
     }
+
+    say "You got $total out of 10 correct";
+    say "Type yes to go again: ";
+    chomp (my $input = <STDIN>);
+    $again = 0 unless $input =~ /yes/i;
 }
 
-say "You got $total out of 10 correct";
