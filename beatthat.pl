d267fabd8935562e09bc2e6cdf6cfa26e8d0cd3a
Simon <simon@localhost>
2014-04-15 14:51:54 +0100

    incorrect answers shown#

--- beatthat.pl
+++ beatthat.pl
@@ -15,9 +15,11 @@ my $clear_string = $terminal->Tputs('cl');
 
 print $clear_string;
 
+my @incorrect_answers;
+
 while ( $again == 1 ) {
     say
-      "****************************** BEAT THAT ******************************";
+    "****************************** BEAT THAT ******************************";
     for ( 1 .. 10 ) {
 
         my $rand1  = int( rand(10) + 1 );
@@ -31,13 +33,24 @@ while ( $again == 1 ) {
                 $valid_input = 1;
                 if ( $input == $answer ) {
                     $total++;
-
                 }
+                else {
+                    push @incorrect_answers, "$rand1 times $rand2 = " . ($rand1 * $rand2);
+                }
+
             }
         }
         $valid_input = 0;
     }
     say "You got $total out of 10 correct";
+    if (@incorrect_answers > 0 ) {
+        foreach my $wrong_answer (@incorrect_answers){
+            print $wrong_answer . "\n";
+        }
+        undef @incorrect_answers;
+    }
+
+
     say "Type yes to go again: ";
     chomp( my $input = <STDIN> );
     $total = 0;
